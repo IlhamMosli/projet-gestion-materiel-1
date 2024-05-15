c326efce37aaf4a404255dfd6f4498a91e44018d
AbdelmoulaMoukrime <mkrabde@gmail.com>
2024-05-15 23:59:33 +0100

    AjouterInterventionServlet

--- src/java/controlle/AjouterInterventionServlet.java
+++ src/java/controlle/AjouterInterventionServlet.java
@@ -6,6 +6,7 @@ package controlle;
 import java.io.IOException;
 import java.io.PrintWriter;
 import DAOClasse.FournisseurDAO;
+import DAOClasse.InterventionDAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -17,68 +18,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
-/**
- *
- * @author AbdelmoulaMoukrime
- */
 @WebServlet(name = "AjouterIntervention", urlPatterns = {"/AjouterIntervention"})
 public class AjouterInterventionServlet extends HttpServlet {
 
-
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AjouterIntervention</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AjouterIntervention at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+        
+        String serie = request.getParameter("serieI");
+        String nom = request.getParameter("nomI");
+        String date = request.getParameter("dateI");
+        String telephone = request.getParameter("telephoneI");
+        String description = request.getParameter("descriptionI");
+        String solution = request.getParameter("solutionI");
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        // Insérer les données dans la base de données
+        InterventionDAO.insertIntervention(serie, nom, date, telephone, description, solution);
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+       response.sendRedirect("Vue/intervention.jsp");
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    }}
+  
 
-}
+   
